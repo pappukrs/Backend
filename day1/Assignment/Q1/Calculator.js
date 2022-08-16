95893f12d8a685da97360c80560f9aa6d3cc74e5
Pappu Kumar <digitalpappu101@gmail.com>
2022-08-16 09:33:44 +0530

    Q1 node basic completed

--- day1/Assignment/Q1/Calculator.js
+++ day1/Assignment/Q1/Calculator.js
@@ -15,5 +15,16 @@ const mul=(a,b)=>{
 const divid=(a,b)=>{
     return a/b;
 }
+const sin=(a)=>{
+    return Math.sin(a)
+}
+const cos=(a)=>{
+    return Math.cos(a)
+}
+const tan=(a)=>{
+    return Math.tan(a)
+}
 
-module.exports={add,sub,mul,divid};
+
+
+module.exports={add,sub,mul,divid,sin,cos,tan};
--- day1/Assignment/Q1/index.js
+++ day1/Assignment/Q1/index.js
@@ -2,7 +2,7 @@ const process=require('process');
 command=process.argv;
 // console.log(command);
 
-const {add,sub,mul,divid}=require("./Calculator");
+const {add,sub,mul,divid,sin,cos,tan}=require("./Calculator");
 const{Ran}=require("./Random");
 
 if(command[2]==="add"){
@@ -22,4 +22,14 @@ else if(command[2]==="sub"){
  }
  else if(command[2]==="Ran"){
     console.log(Ran(+command[3],+command[4]));
+ }
+
+ else if(command[2]==="sin"){
+    console.log(sin(+command[3],+command[4]));
+ }
+ else if(command[2]==="cos"){
+    console.log(cos(+command[3],+command[4]));
+ }
+ else if(command[2]==="tan"){
+    console.log(tan(+command[3],+command[4]));
  }
